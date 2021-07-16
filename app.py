809228e2639ae795e36b9301dcb492f0cc95ae51
Cai <ycai24@MGC12FC3XXMD6N.fbpld77.ford.com>
2021-07-16 16:43:40 -0700

    get post working

--- app.py
+++ app.py
@@ -1,22 +1,50 @@
 from flask import Flask, request, redirect
+from flask_sqlalchemy import SQLAlchemy
 from twilio.twiml.messaging_response import MessagingResponse
 
+
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///message.db'
+
+db = SQLAlchemy(app)
+
+class Message(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    msg = db.Column(db.String(), nullable=False)
 
 @app.route("/sms", methods=['GET', 'POST'])
 def sms_reply():
-    """Respond to incoming calls with a simple text message."""
+    # twilio sending msg in
+    if request.method == "POST":
+        # Get the message the user sent our Twilio number
+        body = request.values.get('Body', None)
+        return saveMsg(body)
+    # digit polling last saved msg
+    else:
+        return getLastSavedMsg()
 
-    # Get the message the user sent our Twilio number
-    body = request.values.get('Body', None)
 
-    # Start our TwiML response
-    resp = MessagingResponse()
+    # # Start our TwiML response
+    # resp = MessagingResponse()
+    #
+    # # Add a message
+    # resp.message("The Robots are coming! Head for the hills!")
 
-    # Add a message
-    resp.message("The Robots are coming! Head for the hills!")
+def saveMsg(body):
+    new_msg = Message(msg=body)
+    try:
+        db.session.add(new_msg)
+        db.session.commit()
+        return "Message successfully added to db: " + body
+    except:
+        return "There was an error adding the message"
 
-    return str(resp)
+
+def getLastSavedMsg():
+    lastSavedMsg = Message.query.limit(1).all()
+    db.session.delete(lastSavedMsg)
+    db.session.commit()
+    return lastSavedMsg
 
 if __name__ == "__main__":
     app.run(debug=True)
