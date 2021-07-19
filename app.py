695b4e0b402a610c48c14753cfb69e42bef19f90
Cai <ycai24@MGC12FC3XXMD6N.fbpld77.ford.com>
2021-07-19 12:01:48 -0700

    message list approach

--- app.py
+++ app.py
@@ -1,19 +1,21 @@
 from flask import Flask, request, redirect
-from flask_sqlalchemy import SQLAlchemy
+# from flask_sqlalchemy import SQLAlchemy
 from twilio.twiml.messaging_response import MessagingResponse
 
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///message.db'
 
-db = SQLAlchemy(app)
+msg_list = []
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///message.db'
 
-class Message(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    msg = db.Column(db.String(), nullable=False)
+# db = SQLAlchemy(app)
+#
+# class Message(db.Model):
+#     id = db.Column(db.Integer, primary_key=True)
+#     msg = db.Column(db.String(), nullable=False)
 
-db.create_all()
-db.session.commit()
+# db.create_all()
+# db.session.commit()
 
 @app.route("/sms", methods=['GET', 'POST'])
 def sms_reply():
@@ -23,11 +25,16 @@ def sms_reply():
         body = request.values.get('Body', None)
         resp = MessagingResponse()
         resp.message("The Robots are coming! Head for the hills!")
-        saveMsg(body)
+        # saveMsg(body)
+
+        msg_list.append(body)
         return str(resp)
     # digit polling last saved msg
     else:
-        return getLastSavedMsg()
+        try:
+            return msg_list[-1]
+        except:
+            return ""
 
     # # Start our TwiML response
     # resp = MessagingResponse()
@@ -37,27 +44,26 @@ def sms_reply():
     #
     # return str(resp)
 
+#
+# def saveMsg(body):
+#     new_msg = Message(msg=body)
+#     print(new_msg)
+#     try:
+#         db.session.add(new_msg)
+#         db.session.commit()
+#     except:
+#         pass
 
 
-def saveMsg(body):
-    new_msg = Message(msg=body)
-    print(new_msg)
-    try:
-        db.session.add(new_msg)
-        db.session.commit()
-    except:
-        pass
-
-
-def getLastSavedMsg():
-    try:
-        lastSavedMsg = Message.query.limit(1).first()
-        print(lastSavedMsg)
-        db.session.delete(lastSavedMsg)
-        db.session.commit()
-        return str(lastSavedMsg.msg)
-    except:
-        return ""
+# def getLastSavedMsg():
+#     try:
+#         lastSavedMsg = Message.query.limit(1).first()
+#         print(lastSavedMsg)
+#         db.session.delete(lastSavedMsg)
+#         db.session.commit()
+#         return str(lastSavedMsg.msg)
+#     except:
+#         return ""
 
 if __name__ == "__main__":
     app.run(debug=True)
