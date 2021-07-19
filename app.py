8d75a2f04054def2ae5ae1154aec71b90225f4d2
Cai <ycai24@MGC12FC3XXMD6N.fbpld77.ford.com>
2021-07-19 09:15:42 -0700

    test

--- app.py
+++ app.py
@@ -1,52 +1,63 @@
 from flask import Flask, request, redirect
-# from flask_sqlalchemy import SQLAlchemy
+from flask_sqlalchemy import SQLAlchemy
 from twilio.twiml.messaging_response import MessagingResponse
 
 
 app = Flask(__name__)
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///message.db'
-#
-# db = SQLAlchemy(app)
-#
-# class Message(db.Model):
-#     id = db.Column(db.Integer, primary_key=True)
-#     msg = db.Column(db.String(), nullable=False)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///message.db'
+
+db = SQLAlchemy(app)
+
+class Message(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    msg = db.Column(db.String(), nullable=False)
+
+db.create_all()
+db.session.commit()
 
 @app.route("/sms", methods=['GET', 'POST'])
 def sms_reply():
-    # # twilio sending msg in
-    # if request.method == "POST":
-    #     # Get the message the user sent our Twilio number
-    #     body = request.values.get('Body', None)
-    #     return saveMsg(body)
-    # # digit polling last saved msg
-    # else:
-    #     return getLastSavedMsg()
+    # twilio sending msg in
+    if request.method == "POST":
+        # Get the message the user sent our Twilio number
+        body = request.values.get('Body', None)
+        resp = MessagingResponse()
+        resp.message("The Robots are coming! Head for the hills!")
+        saveMsg(body)
+        return str(resp)
+    # digit polling last saved msg
+    else:
+        return getLastSavedMsg()
 
-    # Start our TwiML response
-    resp = MessagingResponse()
+    # # Start our TwiML response
+    # resp = MessagingResponse()
+    #
+    # # Add a message
+    # resp.message("The Robots are coming! Head for the hills!")
+    #
+    # return str(resp)
 
-    # Add a message
-    resp.message("The Robots are coming! Head for the hills!")
 
-    return str(resp)
 
-# def saveMsg(body):
-#     new_msg = Message(msg=body)
-#     try:
-#         db.session.add(new_msg)
-#         db.session.commit()
-#         return "Message successfully added to db: " + body
-#     except:
-#         return "There was an error adding the message"
-#
-#
-# def getLastSavedMsg():
-#     # lastSavedMsg = Message.query.limit(1).all()
-#     # db.session.delete(lastSavedMsg)
-#     # db.session.commit()
-#     # return lastSavedMsg
-#     return "sss"
+def saveMsg(body):
+    new_msg = Message(msg=body)
+    print(new_msg)
+    try:
+        db.session.add(new_msg)
+        db.session.commit()
+    except:
+        pass
+
+
+def getLastSavedMsg():
+    try:
+        lastSavedMsg = Message.query.limit(1).first()
+        print(lastSavedMsg)
+        db.session.delete(lastSavedMsg)
+        db.session.commit()
+        return str(lastSavedMsg.msg)
+    except:
+        return ""
 
 if __name__ == "__main__":
     app.run(debug=True)
