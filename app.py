d1b2e4904a99141579453fe49d2bbb6d75a8e9c8
Cai <ycai24@MGC12FC3XXMD6N.fbpld77.ford.com>
2021-07-19 20:33:43 -0700

    pls

--- app.py
+++ app.py
@@ -5,27 +5,33 @@ from twilio.twiml.messaging_response import MessagingResponse
 app = Flask(__name__)
 msg_list = []
 
-@app.route("/sms", methods=['GET', 'POST'])
+@app.route("/sms", methods=['POST'])
 def sms_reply():
     # twilio sending msg in
-    if request.method == "POST":
-        # Get the message the user sent our Twilio number
-        body = request.values.get('Body', None)
-        # Automatic reply
-        resp = MessagingResponse()
-        resp.message("ON IT BOSS :)")
-        # Append operator response to list
-        msg_list.append(body)
-        return str(resp)
+    # if request.method == "POST":
+    # Get the message the user sent our Twilio number
+    body = request.values.get('Body', None)
+    # Automatic reply
+    resp = MessagingResponse()
+    resp.message("ON IT BOSS :)")
+    # Append operator response to list
+    msg_list.append(body)
+    return str(resp)
     # digit polling last saved msg
-    else:
-        try:
-            # temp = msg_list[-1]
-            # msg_list = []
-            # return temp
-            return msg_list[-1]
-        except:
-            return ""
+    # else:
+    #     try:
+    #         # temp = msg_list[-1]
+    #         # msg_list = []
+    #         # return temp
+    #         return msg_list[-1]
+    #     except:
+    #         return ""
+
+@app.route("/query", methods=['GET'])
+def query():
+    temp = msg_list[-1]
+    msg_list = []
+    return temp
 
 
 if __name__ == "__main__":
